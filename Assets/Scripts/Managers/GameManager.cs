90f13a983f493a47507756943238e6a9cae16090
Michael <michaelmartincollins@gmail.com>
2020-01-19 15:11:29 -0800

    Added game data structs

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {   
-     
+    //for each list item create a new struct and load it into the games array
+    public BalanceTestGameData[] balanceTestGameDatas;
+    public ObjectTrackingTestGameData[] objectTrackingTestGameDatas;
+    public MemoryTestGameData[] memoryTestGameDatas;
+
+    public struct BalanceTestGameData
+    {
+         public int baselineTestDuration;
+         public int balanceTestDuration;
+         public int recordingGranularity;
+         public bool muteNaration;
+         public bool useMusic;
+     }
+
+     public struct ObjectTrackingTestGameData
+     {
+         public int incorrectAnswersToLose;
+         public int roundDuration;
+         public int numberOfCups;
+     }
+
+     public struct MemoryTestGameData
+     {
+         public int incorrectAnswersToLose;
+         public int roundDuration;
+         public int numberOfShapes;
+         public float objectShowDuration;
+     }
 }
